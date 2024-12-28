5e82a2cec482205a48bbccc10938dbb2dc88622b
Mohammed Minhaj <nijulinux@gmail.com>
2024-12-28 10:21:34 +0530

    feat(auth) signup service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
-@Controller()
-export class AuthController {}
+@Controller('auth')
+export class AuthController {
+  constructor(private autService: AuthService) {}
+
+  @Post('signup')
+  signUp(@Body() dto: AuthDto) {
+    return this.autService.signup(dto);
+  }
+
+  @Post('signin')
+  signIn() {
+    return 'I am signin';
+  }
+}
